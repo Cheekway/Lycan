3bf0ce6819a085ed49d0cb5c1d4a516d0fb50a7a
DarkCaesar <DarkCaesar@DarkCaesars-MacBook-Pro.local>
2022-12-20 16:58:09 -0700

    First Madlib
    Example of my first Madlib on Python commit from my Visual Studio

---
+++ Madlib.py
@@ -0,0 +1,17 @@
+gender = input("Enter Your Gender: ")
+city = input("Enter Your City: ")
+sport = input("Enter Your Sport: ")
+adjective = input("Enter an Adjective: ")
+adjective2 = input("Enter Another Adjective: ")
+verb = input("Enter a Verb: ")
+age = input("Enter Your Age: ")
+verb2 = input("Enter a Verb: ")
+
+
+
+print("As a " + gender ("growing up in the streets of " + city))
+print("I have become very good in the game of " + sport)
+print("I had to be " + adjective  and  + adjective2 (" to understand the game,"))
+print("because I " + verb (" with older kids."))
+print("When I was " + age ("I knew I was ready to " + verb2 (".")))
+
