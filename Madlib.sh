aea67254e7a8195bc29bd0262f64764194777ceb
Chikwe Azinge <darkcizz@Lycan10DevopsMac.local>
2023-03-13 14:55:47 -0600

    Adding Madlib script

---
+++ Madlib.sh
@@ -0,0 +1,24 @@
+#!/bin /bash
+
+echo "Welcome To Lycan World"
+echo "Please don't scroll down to enjoy this game"
+echo "Enter your Gender: "
+read Gender
+echo "Enter Your City: "
+read City
+echo "Enter Your Sport: "
+read Sport
+echo "Enter an Adjective: "
+read Adjective
+echo "Enter Another Adjective: "
+read Adjective2
+echo "Enter a Verb: "
+read Verb
+echo "Enter your Age: "
+read Age
+echo "Enter another Verb2: 
+read Verb2"
+
+echo "As a $Gender growing up in the streets of $City I have become very good in the game of $Sport I had to be $Adjective and $Adjective2" to understand the game, because I am $Verb with older kids.
+When I was $Age I knew I was ready to $Verb2"
+
